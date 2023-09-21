537ee58051eaf35f1154422813434523fda63851
Karthik Palakurthy <karthik.palakurthy95@gmail.com>
2023-09-21 18:28:31 +0100

    cat 2

--- roboshop-shell/catalogue.sh
+++ roboshop-shell/catalogue.sh
@@ -1,3 +1,4 @@
+script_location=$(pwd)
 dnf install https://rpm.nodesource.com/pub_20.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
 dnf install nodejs -y
 useradd roboshop
@@ -7,8 +8,14 @@ cd /app
 unzip /tmp/catalogue.zip
 cd /app 
 npm install 
+
+cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service
+
 systemctl daemon-reload
 systemctl enable catalogue 
 systemctl start catalogue
+
+cp ${script_location}/files/mongo.repo /etc/yum.repos.d/mongo.repo
+
 dnf install mongodb-org-shell -y
 mongo --host localhost </app/schema/catalogue.js
