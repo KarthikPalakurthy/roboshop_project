ab80a1d953c275a8fc748d506cd225517a7596f9
Karthik Palakurthy <karthik.palakurthy95@gmail.com>
2023-09-21 12:20:34 +0100

    shell scripting examples

---
+++ shell/example.sh
@@ -0,0 +1,13 @@
+echo Hello world
+#For Special charecters, we use  quotes
+echo "*** Hello World ***"
+
+# For colours, we use -e , syntax : echo -e "\e[COLmMESSAGE\e[0m"
+# Red(31), Green(32), yellow(33), blue(34), magenta (35), cyan (36)
+
+echo -e "\e[COL31MESSAGE\e[0m"
+echo -e "\e[COL32MESSAGE\e[0m"
+echo -e "\e[COL33MESSAGE\e[0m"
+echo -e "\e[COL34MESSAGE\e[0m"
+echo -e "\e[COL35MESSAGE\e[0m"
+echo -e "\e[COL36MESSAGE\e[0m"
