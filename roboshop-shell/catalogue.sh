ca2de83914df548dd5acfe9fb16f81ba469fda3d
Karthik Palakurthy <karthik.palakurthy95@gmail.com>
2023-09-21 18:24:10 +0100

    cat

---
+++ roboshop-shell/catalogue.sh
@@ -0,0 +1,14 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+dnf install nodejs -y
+useradd roboshop
+mkdir /app 
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
+cd /app 
+unzip /tmp/catalogue.zip
+cd /app 
+npm install 
+systemctl daemon-reload
+systemctl enable catalogue 
+systemctl start catalogue
+dnf install mongodb-org-shell -y
+mongo --host localhost </app/schema/catalogue.js
