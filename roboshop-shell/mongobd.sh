bf06c063c9dc71c6cbd10b16fca3266777caae45
Karthik Palakurthy <karthik.palakurthy95@gmail.com>
2023-09-21 18:17:59 +0100

    mongo

---
+++ roboshop-shell/mongobd.sh
@@ -0,0 +1,7 @@
+script_location = $(pwd)
+cp ${script_location}/files/mongo.repo /etc/yum.repos.d/mongodb.repo
+dnf install mongodb-org -y 
+systemctl enable mongod 
+
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+systemctl restart mongod
