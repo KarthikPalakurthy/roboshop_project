8a19943e9f4cb41c475c174a1467209691a9f2a0
Karthik Palakurthy <karthik.palakurthy95@gmail.com>
2023-09-22 11:37:35 +0100

    .

--- roboshop-shell/mongobd.sh
+++ roboshop-shell/mongobd.sh
@@ -1,21 +1,21 @@
 source common.sh
 
 print_steps "Copying MongoDB repo file"
-cp ${script_location}/files/mongo.repo /etc/yum.repos.d/mongodb.repo
+cp ${script_location}/files/mongo.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
 status_check
 
 print_steps "Installing MongoDB"
-dnf install mongodb-org -y 
+dnf install mongodb-org -y &>>${LOG}
 status_check
 
 print_steps "Enabling MongoDB"
-systemctl enable mongod 
+systemctl enable mongod &>>${LOG}
 status_check
 
 print_steps "Changing MongoDB listen address"
-sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+sed -i -e "s/127.0.0.1/0.0.0.0" /etc/mongod.conf &>>${LOG}
 status_check
 
 print_steps "Starting MongoDB"
-systemctl restart mongod
+systemctl restart mongod &>>${LOG}
 status_check
