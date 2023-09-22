bca8b2f319e7a5062097dbcf4c282455506889d9
Karthik Palakurthy <karthik.palakurthy95@gmail.com>
2023-09-22 08:06:08 +0100

    added log

--- roboshop-shell/frontend.sh
+++ roboshop-shell/frontend.sh
@@ -1,22 +1,23 @@
 script_location=$(pwd)
+LOG=/log/roboshop.log
 
 echo -e "\e[32m Installing Nginx\e[0m"
-dnf install nginx -y 
+dnf install nginx -y  &>>${LOG}
 
 echo -e "\e[32m Removing Old files\e[0m"
-rm -rf /usr/share/nginx/html/* 
+rm -rf /usr/share/nginx/html/* &>>${LOG}
 
 echo -e "\e[32m Downloading frontend Content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
 
 cd /usr/share/nginx/html 
 
 echo -e "\e[32m Extracting frontend content\e[0m"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip  &>>${LOG}
 
 echo -e "\e[32m Copying config files\e[0m"
-cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf 
+cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
 
 echo -e "\e[32m Enabling and starting Nginx\e[0m"
-systemctl enable nginx 
-systemctl restart nginx
+systemctl enable nginx &>>${LOG}
+systemctl restart nginx &>>${LOG}
