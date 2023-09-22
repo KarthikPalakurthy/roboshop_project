2b66b30d8bebd0f4af9a5e3e1b1ab15a209e53ee
Karthik Palakurthy <karthik.palakurthy95@gmail.com>
2023-09-22 10:37:51 +0100

    dront

--- roboshop-shell/frontend.sh
+++ roboshop-shell/frontend.sh
@@ -5,29 +5,63 @@ echo -e "\e[1m Installing Nginx\e[0m"
 dnf install nginx -y  &>>${LOG}
 if [ $? -eq 0 ]; then
     echo -e "\e[32m Successful\e[0m"
+    else
+    echo -e "\e[31m Failure\e[0m"
+    echo -e "Refer Log file for more info, Log: ${LOG}"
 fi
 
-# if [ $? -eq 0 ]; then
-#     echo -e "\e[1;32mSUCCESS\e[0m"
-#   else
-#     echo -e "\e[1;31mFAILURE\e[0m"
-#     echo "Refer Log file for more information, LOG - ${LOG}"
-# fi
-
-echo -e "\e[32m Removing Old files\e[0m"
+echo -e "\e[1m Removing Old files\e[0m"
 rm -rf /usr/share/nginx/html/* &>>${LOG}
+if [ $? -eq 0 ]; then
+    echo -e "\e[32m Successful\e[0m"
+    else
+    echo -e "\e[31m Failure\e[0m"
+    echo -e "Refer Log file for more info, Log: ${LOG}"
+fi
 
-echo -e "\e[32m Downloading frontend Content\e[0m"
+echo -e "\e[1m Downloading frontend Content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
+if [ $? -eq 0 ]; then
+    echo -e "\e[32m Successful\e[0m"
+    else
+    echo -e "\e[31m Failure\e[0m"
+    echo -e "Refer Log file for more info, Log: ${LOG}"
+fi
 
 cd /usr/share/nginx/html 
 
-echo -e "\e[32m Extracting frontend content\e[0m"
+echo -e "\e[1m Extracting frontend content\e[0m"
 unzip /tmp/frontend.zip  &>>${LOG}
+if [ $? -eq 0 ]; then
+    echo -e "\e[32m Successful\e[0m"
+    else
+    echo -e "\e[31m Failure\e[0m"
+    echo -e "Refer Log file for more info, Log: ${LOG}"
+fi
 
-echo -e "\e[32m Copying config files\e[0m"
+echo -e "\e[1m Copying config files\e[0m"
 cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
+if [ $? -eq 0 ]; then
+    echo -e "\e[32m Successful\e[0m"
+    else
+    echo -e "\e[31m Failure\e[0m"
+    echo -e "Refer Log file for more info, Log: ${LOG}"
+fi
 
-echo -e "\e[32m Enabling and starting Nginx\e[0m"
+echo -e "\e[1m Enabling and starting Nginx\e[0m"
 systemctl enable nginx &>>${LOG}
+if [ $? -eq 0 ]; then
+    echo -e "\e[32m Successful\e[0m"
+    else
+    echo -e "\e[31m Failure\e[0m"
+    echo -e "Refer Log file for more info, Log: ${LOG}"
+fi
+
+echo -e "\e[1m Starting Nginx\e[0m"
 systemctl restart nginx &>>${LOG}
+if [ $? -eq 0 ]; then
+    echo -e "\e[32m Successful\e[0m"
+    else
+    echo -e "\e[31m Failure\e[0m"
+    echo -e "Refer Log file for more info, Log: ${LOG}"
+fi
